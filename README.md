17a58c7f0e62bb059b0d3afe52bd100bb842fb07
Tony Stark <hello@comparecdns.com>
2021-06-04 16:46:18 -0500

    Committing test.py, the core script for how speed tests are going to be done

---
+++ README.md
@@ -0,0 +1,81 @@
+# How to run speed test.
+
+## the image to use
+
+The image you should probably use is: cf_219kb.png
+
+cf_219kb.png is an image that won't be compressed by Jetpack Wordpress. So,
+uploading that image to both Jetpack and CloudFront CDN will result in the same
+image being downloaded.
+
+curl-format.txt is how to run the speed test.
+
+Here is a shell script of how the speed test is run:
+
+## the curl command
+
+```
+speedtest() {
+  curl -w "@curl-format.txt" -o /dev/null -s $@
+}
+```
+
+## the measurement
+
+the very first curl you should copy-paste into your notes. This curl is very
+interesting to measure because the 1st curl is always the slowest. This is
+usually the biggest variance between the CDNs. The first curl is an indicator
+of how fast the CDN loads for 1st-time visitors.
+
+the 2nd measurement is doing at least 10 subsequent curls, and taking the average.
+This measures how well a CDN caches content for multiple page visitors.
+# The script
+
+## Installation:
+The script uses the pycurl module. To install it run:
+
+```
+$ pip3 install pycurl
+```
+
+If the installation fails you may need to install these dependencies:
+- libssl-dev 
+- libcurl4-openssl-dev
+On Linux:
+
+```
+$ sudo apt-get install libssl-dev libcurl4-openssl-dev
+```
+
+## Testing:
+
+To run it just run:
+
+```
+$python3 script.py < url >
+```
+### Example:
+```
+$ python3 script.py 'https://d3va53q3li7xt1.cloudfront.net/wp-content/uploads/2021/05/shoeb-1024x576.png'
+```
+#### Output:
+```
+10 requests done. Average:
+time_namelookup: 0.004422
+time_connect: 0.014404
+time_appconnect: 0.036274900000000006
+time_pretransfer: 0.03636879999999999
+time_redirect: 0.0
+time_starttransfer: 0.049656500000000006
+time_total: 0.0496897
+```
+
+# The other files
+
+```
+cf.png is 703KB.
+wp.png is 329KB.
+```
+
+Jetpack Wordpress compressed cf.png down to wp.png, which is why Jetpack won
+the initial speed test.
---
+++ test.py
@@ -0,0 +1,84 @@
+import pycurl
+import sys
+ 
+if len(sys.argv) != 2:
+    raise ValueError('Please provide a url')
+url = sys.argv[1]
+
+def curl(url):
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.NOBODY, 1)
+    c.perform()
+    return c
+
+def printresults(results):
+    print('time_namelookup: {0}'.format(results[0]))
+    print('time_connect: {0}'.format(results[1]))
+    print('time_appconnect: {0}'.format(results[2]))
+    print('time_pretransfer: {0}'.format(results[3]))
+    print('time_redirect: {0}'.format(results[4]))
+    print('time_starttransfer: {0}'.format(results[5]))
+    print('time to download: {0}'.format(results[6]))
+    print('time_total: {0}'.format(results[7]))
+
+# Turn data from cumulative seconds to individual seconds
+def fixdata(D):
+  D[6] -= D[5]
+  D[5] -= D[3]
+  D[3] -= D[2]
+  D[2] -= D[1]
+  D[1] -= D[0]
+  return D
+
+
+print('-------------------------------------------------------------')
+print('Testing "1st Download Speed"')
+print('-------------------------------------------------------------')
+c = curl(url)
+data = [c.getinfo(c.NAMELOOKUP_TIME),
+        c.getinfo(c.CONNECT_TIME),
+        c.getinfo(c.APPCONNECT_TIME),
+        c.getinfo(c.PRETRANSFER_TIME),
+        c.getinfo(c.REDIRECT_TIME),
+        c.getinfo(c.STARTTRANSFER_TIME),
+        c.getinfo(c.TOTAL_TIME),
+        c.getinfo(c.TOTAL_TIME) ]
+data = fixdata(data)
+printresults(data)
+
+print('-------------------------------------------------------------')
+print('Testing "Cached Downloads Speeds"')
+print('-------------------------------------------------------------')
+
+# url = "https://d3va53q3li7xt1.cloudfront.net/wp-content/uploads/2021/05/shoeb-1024x576.png"
+n = 10
+
+responses = []
+for i in range(n):
+    c = curl(url)
+    data = [c.getinfo(c.NAMELOOKUP_TIME),
+        c.getinfo(c.CONNECT_TIME),
+        c.getinfo(c.APPCONNECT_TIME),
+        c.getinfo(c.PRETRANSFER_TIME),
+        c.getinfo(c.REDIRECT_TIME),
+        c.getinfo(c.STARTTRANSFER_TIME),
+        c.getinfo(c.TOTAL_TIME),
+        c.getinfo(c.TOTAL_TIME) ]
+    data = fixdata(data)
+
+    responses.append(data)
+    c.close()
+
+
+total_sum = [0,0,0,0,0,0,0,0]
+for response in responses:
+    for i in range(len(response)):
+       total_sum[i] = total_sum[i] + response[i] 
+        
+results = [0,0,0,0,0,0,0,0]
+for i in range(len(total_sum)):
+    results[i] = total_sum[i]/n
+
+print('{0} requests done. Average:'.format(n))
+printresults(results)
